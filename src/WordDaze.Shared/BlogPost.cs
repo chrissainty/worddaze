1f7d23cb0017b1e3c043c281abf15f544bef9dfa
Chris Sainty <chrissainty@users.noreply.github.com>
2018-09-01 17:08:59 +0100

    Listing Posts - Developed the home page to display a list of blog posts

---
+++ src/WordDaze.Shared/BlogPost.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace WordDaze.Shared
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime Posted { get; set; }
+        public string Post { get; set; }
+        public string PostSummary 
+        { 
+            get {
+                if (Post.Length > 50)
+                    return Post.Substring(0, 50);
+
+                return Post;
+            }
+        }
+    }
+}
